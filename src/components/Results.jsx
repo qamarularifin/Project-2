30cd71b666088feb926d1377df5342fb3aabac1a
qamarularifin <qamarulafro89@gmail.com>
2021-11-06 20:29:50 +0800

    fixed textSnippet issue

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -24,7 +24,7 @@ const images = searchResults.map((ele, i)=>{
             
             payload: {book_image: ele.volumeInfo.imageLinks.thumbnail,
                      title: ele.volumeInfo.title,
-                     description: ele.searchInfo.textSnippet}
+                     description: ele.searchInfo ? ele.searchInfo.textSnippet : ""}
             
             })}}>Add to Fav</button>
             
