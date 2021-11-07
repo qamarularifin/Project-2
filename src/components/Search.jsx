1801e58c2e173370062a1be2080a01a9bd659e78
qamarularifin <qamarulafro89@gmail.com>
2021-11-07 15:29:33 +0800

    able to search by book count

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,9 +10,6 @@ const Search = (props) =>{
     const [disableCount, setDisableCount] = useState(false)
     console.log("resultsCount", resultsCount)
 
-    // const countDisabled = () =>{
-    //     resultsCount > 41 ? prompt("Must be less than 40!") : ""
-    // }
  
     return(
         <div className="search">
@@ -21,15 +18,15 @@ const Search = (props) =>{
                     <label></label>
                     <input type="text" placeholder="Enter a book title" onChange={handleChange}/>
                     <Link to="/results"> <button type="submit" onClick={handleSubmit} disabled={disableSubmit}>Submit</button> </Link>
-                    <input type="text" placeholder="No. of books (max: 40)"
-                    ref={countRef} onChange={()=>setResultsCount(countRef.current.value)} />
+                    {/* <input type="text" placeholder="No. of books (max: 40)"
+                    ref={countRef} onChange={()=>setResultsCount(countRef.current.value)} /> */}
 
-                    {/* <select>
-                        <option value={resultsCount} onChange={(e)=>setResultsCount(e.target.value)}>10</option>
-                        <option value={resultsCount}  onChange={(e)=>setResultsCount(e.target.value)}>20</option>
-                        <option value={resultsCount}  onChange={(e)=>setResultsCount(e.target.value)}>30</option>
-                        <option value={resultsCount}  onChange={(e)=>setResultsCount(e.target.value)}>40</option>
-                    </select> */}
+                    <select name="option" onChange={(e) => setResultsCount(e.target.value)}>
+                        <option value="10" >10</option>
+                        <option value="20"  >20</option>
+                        <option value="30"  >30</option>
+                        <option value="40"  >40</option>
+                    </select>
                 </div>
          </form>
         </div>
