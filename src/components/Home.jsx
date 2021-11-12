c7836cfccb0f4a2eaabd3f8cde2b66ad0e324dc7
qamarularifin <qamarulafro89@gmail.com>
2021-11-12 17:06:57 +0800

    added carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,19 @@ import {useState} from 'react'
 import SearchBox from './SearchBox'
 import { BookContext } from '../contexts/dataContext'
 
+//Carousel
+import ReactDom from "react-dom"
+import Carousel from "react-elastic-carousel"
+import Item from '../carouselStyle'
+
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
 
 const Home = () =>{
 
@@ -14,7 +27,20 @@ const Home = () =>{
                 <SearchBox />
 
             <div className="carousel">
-                <h2>put carousel here</h2>
+                <h2>Best Seller</h2>
+          
+                            <div className="App">
+                                <Carousel breakPoints={breakPoints}>
+                                <Item>One</Item>
+                                <Item>Two</Item>
+                                <Item>Three</Item>
+                                <Item>Four</Item>
+                                <Item>Five</Item>
+                                <Item>Six</Item>
+                                <Item>Seven</Item>
+                                <Item>Eight</Item>
+                                </Carousel>
+                            </div>
             </div>
 
                 
