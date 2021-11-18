db2ba6486529dbda7a1a6cacfb0097bcfd9cd88d
qamarularifin <qamarulafro89@gmail.com>
2021-11-18 22:46:26 +0800

    added animation for author search

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -11,20 +11,42 @@ const Collapsible = (props) =>{
 
     return(
         <div className="collapsible">
-            <Button className="toggle" style={{backgroundColor: "white"}} startIcon={<SearchIcon/>}  onClick={()=>setIsOpen(!isOpen)}>Search by Author</Button>
-           { isOpen && <div className="content">
+            <Button className="toggle" style={{backgroundColor: "white"}} onClick={()=>setIsOpen(!isOpen)}>Search by Author</Button>
+           <div className={isOpen ? "content show" : "content"}>
 
            <input className="author"type="text" placeholder="Enter by Author" ref={authorSearchRef}/>
                 <div>   
-            <Link to="/results"> <Button variant="contained" color="primary" startIcon={<SearchIcon/>}  type="submit" onClick={handleSubmitOthers} >Search Author</Button> </Link>
+            <Link to="/results"> <Button variant="contained" color="primary" startIcon={<SearchIcon/>}  type="submit" onClick={handleSubmitOthers} >Author</Button> </Link>
                 </div>   
                
 
 
-           </div>  }
+           </div>  
         
         </div>
     )
 }
 
 export default Collapsible;
+
+
+
+
+//backup
+
+// return(
+//     <div className="collapsible">
+//         <button className="toggle"  startIcon={<SearchIcon/>}  onClick={()=>setIsOpen(!isOpen)}>Search by Author</button>
+//        { isOpen && <div className="content">
+
+//        <input className="author"type="text" placeholder="Enter by Author" ref={authorSearchRef}/>
+//             <div>   
+//         <Link to="/results"> <Button variant="contained" color="primary" startIcon={<SearchIcon/>}  type="submit" onClick={handleSubmitOthers} >Author</Button> </Link>
+//             </div>   
+           
+
+
+//        </div>  }
+    
+//     </div>
+// )
